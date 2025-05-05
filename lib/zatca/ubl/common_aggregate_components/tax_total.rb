b4bfa51621aa6be3c3a1fc5c1045e4914f2483ce
Hassan Bawazir <iiops@outlook.com>
2025-05-05 03:46:12 +0300

    Refactor TaxTotal init with dynamic subtotals
    Refactored the `TaxTotal` class to better support both single and multiple <cac:TaxSubtotal> components.
    
    - Automatically generates a TaxSubtotal if `taxable_amount`, `tax_subtotal_amount`, and `tax_category` are present and no explicit subtotals are passed.
    - Removed deprecated `tax_subtotal_element` method.
    - Cleaned up initialization logic and ensured backwards compatibility.
    
    This enhances clarity, reduces redundancy, and aligns with the new `TaxSubtotal` structure.

--- lib/zatca/ubl/common_aggregate_components/tax_total.rb
+++ lib/zatca/ubl/common_aggregate_components/tax_total.rb
@@ -1,45 +1,58 @@
 class ZATCA::UBL::CommonAggregateComponents::TaxTotal < ZATCA::UBL::BaseComponent
   # <cac:TaxTotal>
-  # <cbc:TaxAmount currencyID="SAR">144.9</cbc:TaxAmount>
-  # <cac:TaxSubtotal>
-  #     <cbc:TaxableAmount currencyID="SAR">966.00</cbc:TaxableAmount>
-  #     <cbc:TaxAmount currencyID="SAR">144.90</cbc:TaxAmount>
+  #   <cbc:TaxAmount currencyID="SAR">97.5</cbc:TaxAmount>
+  #   <cac:TaxSubtotal>
+  #     <cbc:TaxableAmount currencyID="SAR">650.0</cbc:TaxableAmount>
+  #     <cbc:TaxAmount currencyID="SAR">97.5</cbc:TaxAmount>
   #     <cac:TaxCategory>
-  #         <cbc:ID schemeAgencyID="6" schemeID="UN/ECE 5305">S</cbc:ID>
-  #         <cbc:Percent>15.00</cbc:Percent>
-  #         <cac:TaxScheme>
-  #             <cbc:ID schemeAgencyID="6" schemeID="UN/ECE 5153">VAT</cbc:ID>
-  #         </cac:TaxScheme>
+  #       <cbc:ID schemeAgencyID="6" schemeID="UN/ECE 5305">S</cbc:ID>
+  #       <cbc:Percent>15</cbc:Percent>
+  #       <cac:TaxScheme>
+  #         <cbc:ID schemeAgencyID="6" schemeID="UN/ECE 5153">VAT</cbc:ID>
+  #       </cac:TaxScheme>
   #     </cac:TaxCategory>
-  # </cac:TaxSubtotal>
+  #   </cac:TaxSubtotal>
+  #   <cac:TaxSubtotal>
+  #     <cbc:TaxableAmount currencyID="SAR">650.0</cbc:TaxableAmount>
+  #     <cbc:TaxAmount currencyID="SAR">0.00</cbc:TaxAmount>
+  #     <cac:TaxCategory>
+  #       <cbc:ID schemeAgencyID="6" schemeID="UN/ECE 5305">Z</cbc:ID>
+  #       <cbc:Percent>0.00</cbc:Percent>
+  #       <cbc:TaxExemptionReasonCode>VATEX-SA-32</cbc:TaxExemptionReasonCode>
+  #       <cbc:TaxExemptionReason>Zero rated due to exemption</cbc:TaxExemptionReason>
+  #       <cac:TaxScheme>
+  #         <cbc:ID schemeAgencyID="6" schemeID="UN/ECE 5153">VAT</cbc:ID>
+  #       </cac:TaxScheme>
+  #     </cac:TaxCategory>
+  #   </cac:TaxSubtotal>
   # </cac:TaxTotal>
-
   def initialize(
-    tax_amount:, tax_subtotal_amount: nil, taxable_amount: nil,
-    rounding_amount: nil, tax_category: nil, currency_id: "SAR"
+    tax_amount:,tax_subtotal_amount: nil, rounding_amount: nil, currency_id: "SAR", tax_category: nil, taxable_amount: nil, tax_subtotals: []
   )
     super()
-
     @tax_amount = tax_amount
-    @tax_subtotal_amount = tax_subtotal_amount
-    @taxable_amount = taxable_amount
     @rounding_amount = rounding_amount
     @tax_category = tax_category || ZATCA::UBL::CommonAggregateComponents::TaxCategory.new
     @currency_id = currency_id
+    @taxable_amount = taxable_amount
+    @tax_subtotal_amount = tax_subtotal_amount
+
+    @tax_subtotals = if @taxable_amount.present? && @tax_subtotal_amount.present? && @tax_category.present? && tax_subtotals.blank?
+      [
+        ZATCA::UBL::CommonAggregateComponents::TaxSubtotal.new(
+          taxable_amount: @taxable_amount,
+          tax_amount: @tax_subtotal_amount,
+          tax_category: @tax_category,
+          currency_id: @currency_id
+        )
+      ]
+    else
+      tax_subtotals
+    end
   end
 
   def name
     "cac:TaxTotal"
-  end
-
-  def tax_subtotal_element
-    if @taxable_amount.present? && @tax_subtotal_amount.present? && @tax_category.present?
-      ZATCA::UBL::BaseComponent.new(name: "cac:TaxSubtotal", elements: [
-        ZATCA::UBL::BaseComponent.new(name: "cbc:TaxableAmount", value: @taxable_amount, attributes: {"currencyID" => @currency_id}),
-        ZATCA::UBL::BaseComponent.new(name: "cbc:TaxAmount", value: @tax_subtotal_amount, attributes: {"currencyID" => @currency_id}),
-        @tax_category
-      ])
-    end
   end
 
   def rounding_amount_element
@@ -50,9 +63,13 @@ class ZATCA::UBL::CommonAggregateComponents::TaxTotal < ZATCA::UBL::BaseComponen
 
   def elements
     [
-      ZATCA::UBL::BaseComponent.new(name: "cbc:TaxAmount", value: @tax_amount, attributes: {"currencyID" => @currency_id}),
+      ZATCA::UBL::BaseComponent.new(
+        name: "cbc:TaxAmount",
+        value: @tax_amount,
+        attributes: { "currencyID" => @currency_id }
+      ),
       rounding_amount_element,
-      tax_subtotal_element
-    ]
+      *@tax_subtotals # each is a TaxSubtotal and responds to `#name` and `#elements`
+    ].compact
   end
 end
